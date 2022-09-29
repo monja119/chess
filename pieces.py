69d6bacf5658fb5904e0751c81185e8c84cb28df
monja199 <monja.sesame@gmail.com>
2022-09-29 17:14:17 +0300

    updating

--- pieces.py
+++ pieces.py
@@ -1,4 +1,5 @@
 import pygame
+import json
 from board import Board
 
 board = Board
@@ -9,6 +10,10 @@ class Pieces:
         # getting window
         window = board().board
 
+        # getting position
+        with open('position.json', 'r') as position:
+            position = json.load(position)
+        print(position["white"])
         # placing pieces
         left = 115
         top = 72
