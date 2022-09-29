c0cbe2ab8efc3a2f1167c4de21f18896f572cd9d
monja199 <monja.sesame@gmail.com>
2022-09-29 16:16:53 +0300

    updating

--- board.py
+++ board.py
@@ -8,22 +8,22 @@ class Board:
 
         # window
         self.bord = pygame.display.set_mode((width, height))
-
         # background
         white = (255, 255, 255)
         black = (0, 0, 0)
         colors = [white, black]
-        left = 0
-        top = 0
-        tile_width = 100
-        tile_height = 72
+        left, top = 0, 0
+        tile_width, tile_height = 100, 72
 
         for i in range(56):
             if i % 8 == 0 and i != 0:
                 top += tile_height
+                left = 0
+                colors = list(reversed(colors))
             pygame.draw.rect(self.bord, colors[i % 2], pygame.Rect(left, top, tile_width, tile_height))
             left += tile_width
 
+        # displaying
         pygame.display.set_caption("Chess Game")
         pygame.display.flip()
 
